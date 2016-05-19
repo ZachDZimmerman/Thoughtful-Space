9b07b74d25abef0428e0cc4a618668eff90a3329
ZachZimmerman7 <zzimmerman@gmail.com>
2016-05-19 11:41:36 -0600

    Adding finishing touches. Share buttons are functional

--- meme.js
+++ meme.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
 
   // Hosting variables to run in functions
   var currentRandomQuote;
+  var currentRandomBackground;
   randomQuote = true;
   getRandomQuote();
   getBackgroundImage();
+  var url= window.location.href;
 
   $('#refresh').click(function() {
     $('.quote').empty();
@@ -22,15 +24,19 @@ $(document).ready(function() {
   })
 
   $('#background').click(function() {
-          randomQuote = false;
-          getBackgroundImage();
-      })
+      randomQuote = false;
+      getBackgroundImage();
+  })
 
-      // $('#screenshot')click(function() {
-      // })
+  $("#twitterBtn").click(function() {
+    window.open("https://twitter.com/intent/tweet?text=Thoughtful+Space+By+@ZacZ&hashtags=SpaceQuotes " + url);
+  });
 
+  $("#facebookBtn").click(function() {
+    window.open("https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fzz-quarter1-project.firebaseapp.com%2F&display=popup&ref=plugin&src=share_button")
+  });
 
-
+// , function(response){});
 
   // Getting Random Quote
   function getRandomQuote() {
@@ -44,23 +50,23 @@ $(document).ready(function() {
   }
 
 
-
-
   //Getting Random Space Image
   function getBackgroundImage() {
-      $.get("https://www.reddit.com/r/spaceporn/top/.json?sort=top&t=all", function(data) {
-          var goodImages = [];
-          // Makes sure images have valid file names
-          for (var i = 0; i < data.data.children.length; i++) {
-              var image = data.data.children[i].data.url;
-              if (image.match(/\.(jpg|jpeg|png|gif|bmp|tiff)$/i)) {
-                  goodImages.push(image);
-              }
+    $.get("https://www.reddit.com/r/spaceporn/top/.json?sort=top&t=all", function(data) {
+      var goodImages = [];
+      // Makes sure images have valid file names
+      for (var i = 0; i < data.data.children.length; i++) {
+          var image = data.data.children[i].data.url;
+          if (image.match(/\.(jpg|jpeg|png|gif|bmp|tiff)$/i)) {
+              goodImages.push(image);
           }
-          // console.log(goodImages);
-          var random = Math.floor(Math.random() * goodImages.length);
-          // console.log(random);
-          $('body').css('background-image', 'url(' + goodImages[random] + ')');
-      })
+      }
+      // console.log(goodImages);
+      var random = Math.floor(Math.random() * goodImages.length);
+      // console.log(random);
+      var currentRandomBackground = goodImages[random];
+      // console.log(currentRandomBackground)
+      $('body').css('background-image', 'url(' + currentRandomBackground + ')');
+    })
   }
 });
